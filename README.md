d6aebc75c0eb48041d20988399c2f767b8623aae
vitali87 <eheva87@gmail.com>
2022-01-12 00:14:06 +0000

    Added math booleans. Structure for calling class methods also changed

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # python-CLI
 A collection of useful python-to-CLI codes.
 
-For example, you can try running:
+For example, you can try running directly:
 
-``python py_to_cli.py average --iterable=[1,2,3,4.5]``
+``python py_to_cli.py AverageList average --iterable=[1,2,3,4.5]``
+
+Or you can add the following line into your ``~/.bashrc``:
+
+``alias py2cli='python /path/to/your/py_to_cli.py'``
+
+Now you can use this syntax:
+
+``py2cli AverageList average --iterable=[1,2,3,4.5]``
--- py_to_cli.py
+++ py_to_cli.py
@@ -2,10 +2,29 @@ import fire
 
 from typing import List
 
+
 class AverageList(List):
     @property
     def average(self) -> float:
-        return sum(self)/ len(self)
+        return sum(self) / len(self)
+
+
+class Is(object):
+
+    @staticmethod
+    def prime(number: int) -> bool:
+        flag = True
+        if number > 1:
+            for i in range(2, number):
+                if number % i == 0:
+                    flag = False
+                    break
+        return flag
+
+    @staticmethod
+    def even(number: int) -> bool:
+        return number % 2 == 0
+
 
 if __name__ == '__main__':
-  fire.Fire(AverageList)
+    fire.Fire()
